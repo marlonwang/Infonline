dc51e724099efbdcb16555d35c15866cbe57d0a9
marlonwang <marlonwang@163.com>
2015-01-28 21:53:15 +0800

    modify SignUp.java

--- src/com/info/servlet/SignUp.java
+++ src/com/info/servlet/SignUp.java
@@ -34,7 +34,6 @@ public class SignUp extends HttpServlet {
 		// TODO Auto-generated method stub
 		doPost(request,response);
 	}
-
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
